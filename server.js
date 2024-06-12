b882631ec1507f33c3616d9fe0dbc8e92180ddb6
arvindydv <arvindydv03@gmail.com>
2024-06-12 16:11:48 +0530

    update server file

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 
-// import swaggerUi from "swagger-ui-express";
-// import swaggerDocument from "./swagger.json" assert { type: "json" };
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // import api routes
 import categoryRouter from "./routes/category.routes.js";
